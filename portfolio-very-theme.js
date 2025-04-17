7d6f9d02f73d24fa64f4c50878c1a9b94d3bf6dd
paigehohman <pjh5761@psu.edu>
2025-04-17 16:19:39 -0400

    commit check in 1

--- portfolio-very-theme.js
+++ portfolio-very-theme.js
@@ -6,33 +6,111 @@ import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
+//PAIGE, YOU ADDED THIS IN
+import "@haxtheweb/scroll-button/scroll-button.js";
+
+<scroll-button></scroll-button>;
+
 /**
  * `portfolio-very-theme`
- * 
+ *
  * @demo index.html
  * @element portfolio-very-theme
  */
 export class PortfolioVeryTheme extends DDDSuper(I18NMixin(LitElement)) {
-
   static get tag() {
     return "portfolio-very-theme";
   }
 
   constructor() {
     super();
-    this.title = "";
     this.t = this.t || {};
     this.t = {
       ...this.t,
-      title: "Title",
+      haxTheClub: "HAX The Club",
+      skip: "Skip",
     };
+    this.active = {};
+    this.skipped = false;
     this.registerLocalization({
       context: this,
       localesPath:
-        new URL("./locales/portfolio-very-theme.ar.json", import.meta.url).href +
+        new URL("./locales/hax-the-club.ar.json", import.meta.url).href +
         "/../",
       locales: ["ar", "es", "hi", "zh"],
     });
+    this.screen = 0;
+    this.screens = [];
+    this.addEventListener("screen-change", (e) => {
+      let tmp = this.screen + parseInt(e.detail.direction);
+      if (tmp > this.screens.length - 1) {
+        tmp = this.screens.length - 1;
+      }
+      if (tmp < 0) {
+        tmp = 0;
+      }
+      this.screen = tmp;
+    });
+    this.addEventListener("screen-ready", (e) => {
+      this.screens = [...this.screens, e.detail.screen];
+    });
+  }
+
+  firstUpdated(changedProperties) {
+    if (super.firstUpdated) {
+      super.firstUpdated(changedProperties);
+    }
+    // use hash in URL
+    if (parseInt(globalThis.location.hash.replace("#", "")) >= 0) {
+      this.screen = parseInt(globalThis.location.hash.replace("#", ""));
+    }
+  }
+
+  // Lit reactive properties
+  static get properties() {
+    return {
+      ...super.properties,
+      screen: { type: Number, reflect: true },
+      screens: { type: Array },
+      skipped: { type: Boolean, reflect: true },
+      active: { type: Object },
+    };
+  }
+
+  updated(changedProperties) {
+    if (super.updated) {
+      super.updated(changedProperties);
+    }
+    // if screen changes, update the hash
+    if (
+      this.shadowRoot &&
+      (changedProperties.has("screens") || changedProperties.has("screen")) &&
+      this.screens.length > 0
+    ) {
+      globalThis.location.hash = this.screen;
+      // scroll the screen into view
+      let active = this.screens.find((screen) => screen.sid == this.screen);
+      if (active) {
+        this.screens.map((screen) => {
+          if (screen.sid == this.screen) {
+            screen.active = true;
+          } else {
+            screen.active = false;
+          }
+        });
+        this.active = null;
+        this.active = active;
+        this.active.scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
+        });
+        // ensure intro skipped if we jumped to something
+        if (this.screen !== 0) {
+          this.skipIntro();
+        }
+      }
+    }
   }
 
   // Lit reactive properties
@@ -45,31 +123,36 @@ export class PortfolioVeryTheme extends DDDSuper(I18NMixin(LitElement)) {
 
   // Lit scoped styles
   static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--portfolio-very-theme-label-font-size, var(--ddd-font-size-s));
-      }
-    `];
+    return [
+      super.styles,
+      css`
+        :host {
+          display: block;
+          color: var(--ddd-theme-primary);
+          background-color: var(--ddd-theme-accent);
+          font-family: var(--ddd-font-navigation);
+        }
+        .wrapper {
+          margin: var(--ddd-spacing-2);
+          padding: var(--ddd-spacing-4);
+        }
+        h3 span {
+          font-size: var(
+            --portfolio-very-theme-label-font-size,
+            var(--ddd-font-size-s)
+          );
+        }
+      `,
+    ];
   }
 
   // Lit render the HTML
   render() {
-    return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    return html` <div class="wrapper">
+      <h3><span>${this.t.title}:</span> ${this.title}</h3>
+      <slot></slot>
+      <scroll-button></scroll-button>
+    </div>`;
   }
 
   /**
